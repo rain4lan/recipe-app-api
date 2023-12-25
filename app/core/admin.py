8854d8febd8c8fda97608d1a96a87cfa87a23af7
rain4lan <rainer.jakob@gmx.de>
2023-12-25 21:25:17 +0700

    Add: Django admin pages, API doc, tests, migrations

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,42 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    # Best practice to add translation (of headers)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active', 'is_staff', 'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    # Readonly
+    readonly_fields = ['last_login']
+    # Create/add fields
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'password1', 'password2', 'name', 'is_active', 'is_staff', 'is_superuser',
+            )
+        }),
+    )
+
+
+# Register user model using custom UserAdmin as admin class
+admin.site.register(models.User, UserAdmin)
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,49 @@
-from django.db import models # noqa
+"""
+Database models
+"""
+from django.db import models
+from django.contrib.auth.models import(
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        # Pass extra fields optionally to accomodate additional fields (without changing this method)
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        # Password will be encrypted
+        user.set_password(password)
+        # Best practive to pass the db like this
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User model"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # Assign our UserManager to this model
+    objects = UserManager()
+
+    # Set the field that is used as PK
+    USERNAME_FIELD = 'email'
